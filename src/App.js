85ed28f3d6fb2554bbfe780eed7fa60a32766199
hnooh <hakosworld@gmail.com>
2025-02-12 00:24:45 +0300

    deply?

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import Dashboard from "./components/Dashboard";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/" element={<LoginPage />} /> {/* Default route */}
-            </Routes>
-        </Router>
+<HashRouter>
+  <Routes>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/" element={<LoginPage />} /> {/* Default route */}
+  </Routes>
+</HashRouter>
     );
 }
 
